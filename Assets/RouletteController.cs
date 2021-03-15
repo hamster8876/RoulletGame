02cb09ed3d466a3891afec79a7095c2d4b5fb25e
hamster8876 <hamster8876@naver.com>
2021-03-15 15:20:08 +0900

    Initial commit

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class RouletteController : MonoBehaviour
+    {
+        float rotSpeed = 0;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+            this.rotSpeed = 100.0f;
+            }
+            if (Input.GetMouseButtonDown(1))
+        {
+            this.rotSpeed = 0;
+        }
+        transform.Rotate(0, 0, this.rotSpeed);
+        this.rotSpeed *= 0.98f;
+        }
+    }
